f1388cab576722dba42bd10efcf7c91c02dbfe5c
Santiago Marulanda <santiagomm1997@gmail.com>
2023-08-29 00:09:51 +0200

    Map the domain name from env vars in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,42 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    server: {
-        https: {
-            cert: './.certs/tiagocode.tests.pem',
-            key: './.certs/tiagocode.tests.key',
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const appDomain = env.APP_URL.replace(/(http:\/\/)|(https:\/\/)/, '');
+
+    return {
+        server: {
+            https: {
+                cert: `./.certs/${appDomain}.crt`,
+                key: `./.certs/${appDomain}.key`,
+            },
+            host: appDomain,
+            hmr: {
+                host: appDomain,
+            },
+            cors: {
+                origin: '*'
+            }
         },
-        host: 'website.tiagocode.tests',
-        hmr: {
-            host: 'website.tiagocode.tests',
-        },
-        cors: {
-            origin: '*'
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                ssr: 'resources/js/ssr.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            coverage: {
+                reporter: ['html'],
+                reportsDirectory: './resources/js/com/coverage'
+            },
+            setupFiles: ['resources/js/setup.js']
         }
-    },
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            ssr: 'resources/js/ssr.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        coverage: {
-            reporter: ['html'],
-            reportsDirectory: './resources/js/tests/coverage'
-        },
-        setupFiles: ['resources/js/setup.js']
     }
 });
